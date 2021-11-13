b7168641b0bd71a7c21605a9e6ecd3471a8ea60d
Christoph Lohr <clohrffb@gmail.com>
2021-11-13 14:23:40 +0100

    Scene building, blur_far on start

--- Scripts/Start.gd
+++ Scripts/Start.gd
@@ -6,9 +6,11 @@ extends Spatial
 # var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
+	# Non-critical Startup Settings
 	OS.set_window_maximized(true)
+	get_node("WorldEnvironment").environment.set_dof_blur_far_enabled(true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
