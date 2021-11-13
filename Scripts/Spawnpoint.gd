f7c6b6b5b480bd9c1a99339c82aa34cf896d5310
Christoph Lohr <clohrffb@gmail.com>
2021-11-13 12:39:53 +0100

    Added script to respawn new ball

---
+++ Scripts/Spawnpoint.gd
@@ -0,0 +1,23 @@
+extends Position3D
+
+
+func _spawn_new_ball():
+	add_child(load("res://Scenes/Ball.tscn").instance())
+
+func _ready():
+	_spawn_new_ball()
+
+
+
+
+#func _on_Area_Kill_body_entered(body):
+#	if body.get_collision_layer() == 2:
+#		body.queue_free()
+#		_spawn_new_ball()
+
+
+
+func _on_Area_Kill_body_exited(body):
+	if body.get_collision_layer() == 2:
+		body.queue_free()
+		_spawn_new_ball()
